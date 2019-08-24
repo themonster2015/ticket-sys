a6e05e79c1184bb927bc11ae3fa84f4db9611533
themonster2015 <quynhyen.vothi@gmail.com>
2019-08-24 12:31:19 +0200

    generated reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_103005) do
+ActiveRecord::Schema.define(version: 2019_08_24_103059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_08_24_103005) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "ticket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ticket_id"], name: "index_reservations_on_ticket_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -39,5 +48,7 @@ ActiveRecord::Schema.define(version: 2019_08_24_103005) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "tickets"
+  add_foreign_key "reservations", "users"
   add_foreign_key "tickets", "events"
 end
