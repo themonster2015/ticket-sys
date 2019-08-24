6daa300926c1f192c14830510fe7ef2003a8230a
themonster2015 <quynhyen.vothi@gmail.com>
2019-08-24 12:33:42 +0200

    add quantity column to reservations

---
+++ db/migrate/20190824103319_add_quantity_column_to_reservations.rb
@@ -0,0 +1,5 @@
+class AddQuantityColumnToReservations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reservations, :quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_103059) do
+ActiveRecord::Schema.define(version: 2019_08_24_103319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_08_24_103059) do
     t.bigint "ticket_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["ticket_id"], name: "index_reservations_on_ticket_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
